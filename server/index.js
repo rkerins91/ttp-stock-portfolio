90e5dcf4d00c0100ad58d7e07e5e8c9572fd1b80
Ryan Kerins <ryanakerins@gmail.com>
2019-10-01 19:38:59 -0400

    added users moded and get route skeleton

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+
+app.use('/api/users', require('./api/users'))
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('listening on 8080')
